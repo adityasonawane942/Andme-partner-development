8360e2a66d0951522fd634ea8158e1486038433f
Aditya <sadityam123@gmail.com>
2020-04-29 10:18:45 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,53 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './landing/home/home.component';
+import { LoginComponent } from './landing/login/login.component';
+import { ApplyComponent } from './landing/apply/apply.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { UpdatesComponent } from './user/updates/updates.component';
+import { StoreComponent } from './user/store/store.component';
+import { PerformanceComponent } from './user/performance/performance.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    UserComponent,
+    HomeComponent,
+    LoginComponent,
+    ApplyComponent,
+    ProfileComponent,
+    UpdatesComponent,
+    StoreComponent,
+    PerformanceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path:'',component:LandingComponent,
+        children: [
+          {path:'home',component:HomeComponent},
+          {path:'login',component:LoginComponent},
+          {path:'apply',component:ApplyComponent},
+        ]},
+      {path:'user',component:UserComponent,
+        children: [
+          {path:'profile',component:ProfileComponent},
+          {path:'updates',component:UpdatesComponent},
+          {path:'store',component:StoreComponent},
+          {path:'performance',component:PerformanceComponent},
+        ]},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
