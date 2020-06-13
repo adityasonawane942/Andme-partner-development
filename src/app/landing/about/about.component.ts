d53364f309e6063d4fcc23f66536233c1e251bf2
Aditya <sadityam123@gmail.com>
2020-06-13 12:36:36 +0530

    deploy try

--- src/app/landing/about/about.component.ts
+++ src/app/landing/about/about.component.ts
@@ -43,7 +43,7 @@ export class AboutComponent implements OnInit {
 
   register() {
     if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(this.user.username)&&this.user.password==this.confpass) {
-      this.http.post('http://127.0.0.1:8000/users/', this.user, this.httpOptions)
+      this.http.post('http://127.0.0.1:8000/rout/users/', this.user, this.httpOptions)
         .subscribe(
           result => {
             this._ngZone.run(() => this.router.navigate(['/form']));
